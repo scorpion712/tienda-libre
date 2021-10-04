372a03bd3d54f536c298eaf8855072c3630b17a7
ldiez <ldiez@alumnos.unicen.edu.ar>
2021-10-04 19:19:41 -0300

    refactoring backend and create seed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const express = require('express');
+const Product = require('./model/productModel');
 // API routers
 const productRouter = require('./router/productRouter'); 
 
@@ -22,6 +23,14 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use('/api', productRouter);
 //app.use('/api', orderRouter);
   
+const data = require("./data");
+app.get("/api/seed", async (req, res) => {
+    await Product.remove({}); // deprecated
+    const products = await Product.insertMany(data.products);
+    res.send({products});
+    }
+)
+
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || 'localhost';
 
